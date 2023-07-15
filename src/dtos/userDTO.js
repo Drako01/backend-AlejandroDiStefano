2c5e5f570601bd1f3820bffc739836b4521c3133
Alejandro Di Stefano <addistefano@live.com.ar>
2023-07-14 21:35:28 -0300

    Control de Stock + DTO

--- src/dtos/userDTO.js
+++ src/dtos/userDTO.js
@@ -1,10 +1,10 @@
 export default class UserDTO {
     constructor(user) {
-        this.id = user._id
+        this.id = user._id || user.user._id
         this.full_name = `${user.first_name || user.user.first_name} ${user.last_name}`
-        this.email = user.email
-        this.phone = user.phone
-        this.age = user.age
-        this.role = user.role
+        this.email = user.email || user.user.email
+        this.phone = user.phone || user.user.phone
+        this.age = user.age || user.user.age
+        this.role = user.role || user.user.role
     }
 }
