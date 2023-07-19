569435dce21f650c73ca31d3f3b24ca85487a60b
Alejandro Di Stefano <addistefano@live.com.ar>
2023-07-19 15:26:23 -0300

    Pruebas de Loggers

---
+++ src/test/logger.test.js
@@ -0,0 +1,13 @@
+import loggers from '../server/logger.js';
+
+// Inicio de las pruebas
+loggers.debug('Esto es un mensaje de depuración');
+loggers.http('Esto es un mensaje de solicitud HTTP');
+loggers.info('Esto es un mensaje de información');
+loggers.notice('Esto es un mensaje de notificación');
+loggers.warning('Esto es un mensaje de advertencia');
+loggers.error('Esto es un mensaje de error');
+loggers.fatal('Esto es un mensaje de error fatal');
+
+// Fin de las pruebas
+console.log('Pruebas de logs finalizadas');
