b7e5ca9500f34421ed436c0e76c27eb16a094a7f
Alejandro Di Stefano <addistefano@live.com.ar>
2023-07-24 09:38:45 -0300

    Controllers and Routers

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -5,6 +5,33 @@ import loggers from '../config/logger.js'
 
 const cookieName = config.jwt.cookieName;
 
+export const getIndexProductsController = async (req, res) => {
+    try {
+        const limit = parseInt(req.query.limit);
+        const products = await Product.find().lean();
+        const userToken = req.cookies[cookieName];
+        
+        if (!userToken) {
+            res.status(200).render('index', { products: products.slice(0, 4), productLength: products.length, user: null });
+            return;
+        }
+        const user = getUserFromToken(req) ;      
+
+        if (!user) {
+            res.status(200).render('index', { products: products.slice(0, 4), productLength: products.length, user: null });
+            return;
+        }
+
+        if (isNaN(limit)) {
+            res.status(200).render('index', { products: products.slice(0, 4), productLength: products.length, user });
+        } else {
+            res.status(200).render('index', { products: products.slice(0, limit), productLength: products.length, user });            
+        }
+    } catch (err) {
+        loggers.error(err);
+        res.status(500).render({ message: 'Internal server error' });
+    }
+}
 
 let user = null;
 // Obtener todos los productos
--- src/routers/index.router.js
+++ src/routers/index.router.js
@@ -1,39 +1,7 @@
 import { Router } from 'express';
-import Product from '../daos/models/products.model.js';
-import { getUserFromToken } from '../middlewares/user.middleware.js';
-import config from '../config/config.js';
-import loggers from '../config/logger.js'
-
+import { getIndexProductsController } from '../controllers/products.controller.js';
 const router = Router();
-const cookieName = config.jwt.cookieName;
 
-router.get('/', async (req, res) => {
-    try {
-        const limit = parseInt(req.query.limit);
-        const products = await Product.find().lean();
-        const userToken = req.cookies[cookieName];
-        
-        if (!userToken) {
-            res.status(200).render('index', { products: products.slice(0, 4), productLength: products.length, user: null });
-            return;
-        }
-        const user = getUserFromToken(req) ;      
-
-        if (!user) {
-            res.status(200).render('index', { products: products.slice(0, 4), productLength: products.length, user: null });
-            return;
-        }
-
-        if (isNaN(limit)) {
-            res.status(200).render('index', { products: products.slice(0, 4), productLength: products.length, user });
-        } else {
-            res.status(200).render('index', { products: products.slice(0, limit), productLength: products.length, user });
-            
-        }
-    } catch (err) {
-        loggers.error(err);
-        res.status(500).render({ message: 'Internal server error' });
-    }
-});
+router.get('/', getIndexProductsController );
 
 export default router;
