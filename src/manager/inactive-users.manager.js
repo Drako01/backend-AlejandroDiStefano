3a39eeb9b5b5a97c6bd13e64ac99faf14047b090
Alejandro Di Stefano <addistefano@live.com.ar>
2023-08-02 19:16:16 -0300

    Aviso de Usuarios Inactivos x 1 Año

--- src/manager/inactive-users.manager.js
+++ src/manager/inactive-users.manager.js
@@ -15,15 +15,18 @@ export async function findInactiveUsers() {
 
     try {
         const inactiveUsers = await UserService.getAll();
+        console.log('\n') // Salto de linea insertado para que se vea mejor en la consola
+        loggers.notice('Lista de usuarios: ')
         inactiveUsers.forEach((user) => {
             if (user.updatedAt < oneYearAgo) {
                 const formattedDate = formatDate(user.updatedAt);
-                console.log('\n') // Salto de linea insertado para que se vea mejor en la consola
-                loggers.info(`Usuario inactivo: ${user.first_name || user.user.first_name} ${user.last_name}- Último inicio de sesión: ${formattedDate} | Hace más de un año que no se conecta.!!`);
+                loggers.warn(`Usuario inactivo: ${user.first_name || user.user.first_name} ${user.last_name} - Último inicio de sesión: ${formattedDate} | Hace más de un año que no se conecta.!!`);
+            } else {
+                loggers.info(`Usuario activo: ${user.first_name || user.user.first_name} ${user.last_name}`);
             }
         });
     } catch (error) {
         customError(error);
-        loggers.error('Error al buscar usuarios inactivos:');        
+        loggers.error('Error al buscar usuarios inactivos:');
     }
 }
