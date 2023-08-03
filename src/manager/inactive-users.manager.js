598ebad8475f4225bf6a376b42d0e84e3d81af9b
Alejandro Di Stefano <addistefano@live.com.ar>
2023-08-03 14:03:04 -0300

    Aviso de Usuarios Inactivos x 1 Año

--- src/manager/inactive-users.manager.js
+++ src/manager/inactive-users.manager.js
@@ -2,6 +2,7 @@ import customError from '../services/error.log.js';
 import { UserService } from '../repositories/index.js';
 import loggers from '../config/logger.js';
 import UserDTO from '../dtos/user.dto.js';
+import customMessageSessions from '../services/sessions.log.js'
 
 function formatDate(date) {
     const day = String(date.getDate()).padStart(2, '0');
@@ -20,11 +21,16 @@ export async function findInactiveUsers() {
         loggers.notice('Lista de usuarios: ')
         inactiveUsers.forEach((user) => {
             let users = new UserDTO(user);
+            
             if (user.updatedAt < oneYearAgo) {
                 const formattedDate = formatDate(user.updatedAt);
-                loggers.warn(`Usuario inactivo: ${users.full_name} | Último inicio de sesión: ${formattedDate} | Hace más de un año que no se conecta.!!`);
+                let messageA = `Usuario inactivo: ${users.full_name} | Último inicio de sesión: ${formattedDate} | Hace más de un año que no se conecta.!!`
+                customMessageSessions(messageA)
+                loggers.warn(messageA);
             } else {
-                loggers.info(`Usuario activo: ${users.full_name}`);
+                let messageB = `Usuario activo: ${users.full_name}`
+                customMessageSessions(messageB)
+                loggers.info(messageB);
             }
         });
     } catch (error) {
