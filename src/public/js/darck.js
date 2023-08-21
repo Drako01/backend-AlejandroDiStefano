8438d3a8406ea3186993058294acb364ef4c9811
Alejandro Di Stefano <addistefano@live.com.ar>
2023-08-21 19:22:09 -0300

    Dark Mode

--- src/public/js/darck.js
+++ src/public/js/darck.js
@@ -1,11 +1,15 @@
 const switchButton = document.getElementById('switch');
 const cards = document.querySelectorAll('.card');
 const tables = document.querySelectorAll('.table');
+const textos = document.querySelectorAll('.text-center');
+const prducts = document.querySelectorAll('.products');
 
 switchButton.addEventListener('click', () => {
     document.body.classList.toggle('dark');
     cards.forEach(card => card.classList.toggle('dark'));
     tables.forEach(table => table.classList.toggle('table-dark'));
+    textos.forEach(texto => texto.classList.toggle('dark'));
+    prducts.forEach(prduct => prduct.classList.toggle('dark'));
     switchButton.classList.toggle('active');
 
 
@@ -20,5 +24,7 @@ if (localStorage.getItem('darkMode') == 'enabled') {
     document.body.classList.toggle('dark');    
     cards.forEach(card => card.classList.toggle('dark'));
     tables.forEach(table => table.classList.toggle('table-dark'));
+    textos.forEach(texto => texto.classList.toggle('dark'));
+    prducts.forEach(prduct => prduct.classList.toggle('dark'));
     switchButton.classList.toggle('active');
 }
